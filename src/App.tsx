eb25b6b9989f101a163a89a77a988ca42e1b1979
SiaS_32 <109104432+sias32@users.noreply.github.com>
2023-05-29 03:06:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import imgBook from './assets/img/book.png';
 import './assets/style/App.css';
 import Stars from './components/Stars';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <img className="img" src="/src/assets/img/book.png" alt="book" />
+      <img className="img" src={imgBook} alt="book" />
       <div className="info">
         <p className="title">Повелители DOOM</p>
         <Stars count={2} />
